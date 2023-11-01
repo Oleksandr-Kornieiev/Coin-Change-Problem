e9b6310528fb1f83bb3e3ad1e384abf40cc24261
Oleksandr-Kornieiev <kornieiev2021ks11@student.karazin.ua>
2023-11-01 20:57:55 +0200

    adding menu looping for user friendliness, validation for the answer,  sorting of coin denominations,  greedy algorithm and recursive method with memoization (in progress), change in gitignor

--- main.rb
+++ main.rb
@@ -1,40 +1,65 @@
 require_relative "methods.rb"
 
-puts "=== Меню ==="
-while true
-  puts "1. Рекурсивний метод"
-  puts "2. Ітераційний метод"
-  puts "3. Вихід"
-  choice = gets.to_i
-  case choice
-  when 1
-    puts "Введіть суму, яку необхідно розміняти:"
-    amount = gets.to_i
-    puts "Введіть кількість номіналів:"
-    n = gets.to_i
-    coins = []
-    puts "Введіть номінали монет"
-    for x in 1..n
-      puts "#{x} монета:"
-      coins.push(gets.to_i)
+loop do
+  puts "=== Меню ==="
+  while true
+    puts "1. Жадібний метод (первинне помилкове рішення)"
+    puts "2. Ітераційний метод (динамічне програмування)"
+    puts "3. Рекурсивний метод (у розробці)"
+    puts "4. Вихід"
+    choice = gets.to_i
+    case choice
+    when 1
+      puts "Введіть суму, яку необхідно розміняти:"
+      amount = gets.to_i
+      puts "Введіть кількість номіналів:"
+      n = gets.to_i
+      coins = []
+      puts "Введіть номінали монет"
+      for x in 1..n
+        puts "#{x} монета:"
+        coins.push(gets.to_i)
+      end
+      coins = coins.sort.reverse
+      puts "Мінімальна кількість монет для розміну - #{greedy_algorithm(coins, amount)}"
+      break
+    when 2
+      puts "Введіть суму, яку необхідно розміняти:"
+      amount = gets.to_i
+      puts "Введіть кількість номіналів:"
+      n = gets.to_i
+      coins = []
+      puts "Введіть номінали монет"
+      for x in 1..n
+        puts "#{x} монета:"
+        coins.push(gets.to_i)
+      end
+      coins.sort!
+      puts "Мінімальна кількість монет для розміну - #{coin_change_iteration(coins, amount)}"
+      break
+    when 3
+      puts "Метод у розробці"
+=begin
+      puts "Введіть суму, яку необхідно розміняти:"
+      amount = gets.to_i
+      puts "Введіть кількість номіналів:"
+      n = gets.to_i
+      coins = []
+      puts "Введіть номінали монет"
+      for x in 1..n
+        puts "#{x} монета:"
+        coins.push(gets.to_i)
+      end
+      memo = { }
+      puts "Мінімальна кількість монет для розміну - #{coin_change_recursion_with_memo(coins, amount, memo)}"
+      break
+=end
+    when 4
+      puts "Вихід із програми."
+      break
+    else
+      puts "Невірний вибір. Будь ласка, оберіть знову 1, 2, 3 або 4."
     end
-    puts "Мінімальна кількість монет для розміну - #{}"
-  when 2
-    puts "Введіть суму, яку необхідно розміняти:"
-    amount = gets.to_i
-    puts "Введіть кількість номіналів:"
-    n = gets.to_i
-    coins = []
-    puts "Введіть номінали монет"
-    for x in 1..n
-      puts "#{x} монета:"
-      coins.push(gets.to_i)
-    end
-    puts "Мінімальна кількість монет для розміну - #{coin_change_iteration(coins, amount)}"
-  when 3
-    puts "Вихід із програми."
-    break
-  else
-    puts "Невірний вибір. Будь ласка, оберіть знову"
   end
+  break if menu_again == "n"
 end
